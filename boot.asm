e7f8d54669f980ec9b66b2c8055d928dd3319644
Volodymyr Bortniak <>
2025-05-01 21:21:01 +0200

    adds comments

--- boot.asm
+++ boot.asm
@@ -2,24 +2,24 @@ ORG 0x7c00
 BITS 16
 
 start:
-    mov si, message
-    call print
+    mov si, message ;move address of the message to si register
+    call print ;call print subroutin
     jmp $
 
 
 print:
-    mov bx, 0
+    mov bx, 0 ;set bx to 0
 .loop:
-    lodsb
-    cmp al, 0
+    lodsb ;see where si register is pointing
+    cmp al, 0 ;compare al to 0 if yes - done, if no - print_char and loop
     je .done
     call print_char
     jmp .loop
 .done:
-    ret
+    ret ;return from subroutin
 
 print_char:
-    mov ah, 0eh
+    mov ah, 0eh ; 0eh - function for outoutting to the screen when talking to bios
     int 0x10
     ret
 message: db 'Hello World!', 0
